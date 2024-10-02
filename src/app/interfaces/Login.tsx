2bc5beebe13efd4c5493bd8274c68ea279d3a500
JonTheJol <cjcb040404@hotmail.com>
2024-10-01 20:46:03 -0600

    login func

--- src/app/interfaces/Login.tsx
+++ src/app/interfaces/Login.tsx
@@ -1,15 +1,33 @@
-"use client";
-
 import React, { useState } from "react";
+import axios from 'axios';
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const login = (event: React.FormEvent) => {
-    event.preventDefault();
-    console.log("Email:", email);
-    console.log("Password:", password);
+  const  
+ login = async (event: React.FormEvent) => {
+    event.preventDefault();  
+
+
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/api/login', { email, password });
+      const data = response.data;
+
+      // Handle login success (e.g., store token, redirect to another page)
+      console.log('Login successful:', data);
+
+      // Assuming the API returns an access token in the response data
+      localStorage.setItem('accessToken', data.access_token);
+      // Redirect to a protected page
+      window.location.href = '/protected';
+    } catch (error) {
+      // Handle login failure
+      console.error('Login failed:', error.response?.data?.message || 'Unknown error');
+
+      // Display an error message to the user
+      alert('Login failed. Please check your credentials.');
+    }
   };
 
   return (
